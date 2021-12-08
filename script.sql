d82a229876b4875b7875b6e8006b3dce595c3771
noenoenoe93 <79853812+noenoenoe93@users.noreply.github.com>
2021-12-08 12:07:39 +0100

    Update and rename README.md to script.sql

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE PlaylistCollaboratif;
 
 CREATE TABLE utilisateurs (
-user_id user PRIMARY KEY NOT NULL AUTO_INCREMENT,
+user_id serial PRIMARY KEY NOT NULL AUTO_INCREMENT,
 votes int NOT NULL,
 identifiant varchar(20) NOT NULL,
 SuperVote int,
@@ -12,7 +12,7 @@ role_organisateur varchar(10) NOT NULL
 );
 
 CREATE TABLE playlist (
-musique_id musique PRIMARY KEY NOT NULL AUTO_INCREMENT,
+musique_id serial PRIMARY KEY NOT NULL AUTO_INCREMENT,
 votes int NOT NULL,
 duree minute NOT NULL,
 style_musique varchar(15) NOT NULL,
